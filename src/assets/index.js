2d5c289253efdf71603f0a0a3fec2298d92a119f
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-12-08 18:11:41 +0900

    image 추가

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as meatballMenu } from "./meatballMenu.png";
+export { default as checkBox } from "./checkBox.png";
+export { default as checkBoxFilled } from "./chechBoxFilled.png";
+export { default as login } from "./login.png";
+export { default as signup } from "./signup.png";
